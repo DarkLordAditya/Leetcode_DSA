d67e61a8c3c32ca2a7974b98836bca6643c98acd
DarkLordAditya <adityarai36892@gmail.com>
2025-11-23 01:46:06 +0530

    Add files via upload

---
+++ Array/Contner_with_most_water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int area = 0;
+        while (left < right) {
+            int barHeight = min(height[left], height[right]);
+            int width = right - left;
+            area = max(area, barHeight * width);
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return area;
+    }
+};
